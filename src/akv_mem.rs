4503afe47d501a88cd8ea34deee7865192abde5b
pete <p_traunf@pm.me>
2024-05-29 21:23:49 -0400

    get, insert

--- src/akv_mem.rs
+++ src/akv_mem.rs
@@ -40,17 +40,20 @@ fn main() {
     match action {
         "get" => match store.get(key).unwrap() {
             None => eprintln!("{:?} not found", key),
-            Some(value) => println!("{:?}", value)
+            Some(value) => {
+                let string_value = String::from_utf8_lossy(&*value);
+                println!("{}", string_value);
+            }
         }
         "delete" => store.delete(key).unwrap(),
         "insert" => {
             let value = maybe_value.expect(&USAGE).as_ref();
             store.insert(key, value).unwrap()
-        },
+        }
         "update" => {
             let value = maybe_value.expect(&USAGE).as_ref();
             store.update(key, value).unwrap()
-        },
+        }
         _ => eprintln!("{}", &USAGE)
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 use std::collections::HashMap;
 use std::fs::{File, OpenOptions};
 use std::io;
-use std::io::{BufReader, Read, Seek, SeekFrom};
+use std::io::{BufReader, BufWriter, Read, Seek, SeekFrom, Write};
 use std::path::Path;
-use byteorder::{LittleEndian, ReadBytesExt};
+use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
 use crc::crc32;
 use serde::{Deserialize, Serialize};
 
@@ -54,6 +54,9 @@ impl ActionKV {
         }
         Ok(())
     }
+    fn seek_to_end(&mut self) -> io::Result<u64> {
+        self.f.seek(SeekFrom::End(0))
+    }
     fn process_record<R: Read>(f: &mut R) -> io::Result<KeyValuePair> {
         let saved_checksum = f.read_u32::<LittleEndian>()?;
         let key_len = f.read_u32::<LittleEndian>()?;
@@ -74,6 +77,64 @@ impl ActionKV {
         let key = data;
         Ok(KeyValuePair { key, value })
     }
+
+    pub fn get(&mut self, key: &ByteStr) -> io::Result<Option<ByteString>> {
+        let position = match self.index.get(key) {
+            None => return Ok(None),
+            Some(position) => *position,
+        };
+
+        let kv = self.get_at(position)?;
+        Ok(Some(kv.value))
+    }
+   fn get_at(&mut self, position:u64) -> io::Result<KeyValuePair> {
+       let mut f = BufReader::new(&mut self.f);
+       f.seek(SeekFrom::Start(position))?;
+       let kv = ActionKV::process_record(&mut f)?;
+       Ok(kv)
+   }
+
+    // fn find(&mut self, target: &ByteStr) -> io::Result<Option<(u64, ByteString)>> {
+    //
+    // }
+    pub fn insert(&mut self, key: &ByteStr, value: &ByteStr) -> io::Result<()> {
+        let position = self.insert_ignore_index(key, value)?;
+        self.index.insert(key.to_vec(), position);
+        Ok(())
+    }
+
+    fn insert_ignore_index(&mut self, key: &ByteStr, value: &ByteStr) -> io::Result<u64> {
+        let mut f = BufWriter::new(&mut self.f);
+        let key_len = key.len();
+        let val_len = value.len();
+        let mut payload = ByteString::with_capacity(key_len + val_len);
+        for byte in key {
+            payload.push(*byte);
+        }
+        for byte in value {
+            payload.push(*byte)
+        }
+        let checksum = crc32::checksum_ieee(&payload);
+       let next_byte = SeekFrom::End(0);
+        let current_position: u64 = f.seek(SeekFrom::Current(0))?;
+        f.seek(next_byte)?;
+        f.write_u32::<LittleEndian>(checksum).expect("Failed to write checksum");
+        f.write_u32::<LittleEndian>(key_len as u32)?;
+        f.write_u32::<LittleEndian>(val_len as u32)?;
+        f.write_all(&mut payload)?;
+
+        Ok(current_position)
+    }
+
+    #[inline]
+    pub fn update(&mut self, key: &ByteStr, value: &ByteStr) -> io::Result<()> {
+        self.insert(key, value)
+    }
+
+    #[inline]
+    pub fn delete(&mut self, key: &ByteStr) -> io::Result<()> {
+        self.insert(key, b"")
+    }
 }
 
 pub fn about() {
@@ -86,8 +147,8 @@ ActionKV is an implementation of Bitcask, a Log-Structured Hash Table
  ┌───┴───────────────────────┐ ┌────┴───────┐ ┌────┴──────┐
  │checksum  key_len  val_len │ │ key        │ │   value   │
   0 1 2 3   4 5 6 7   8 9 A B
- └─┴─┴─┴─┘ └─┴─┴─┴─┘ └─┴─┴─┴─┘ └─┴─┴...─┴─┴─┘  └─┴─┴...┴─┴─┘
-   u32       u32     u32         [u8; key_len] [u8; value_len]
+ └─┴─┴─┴─┘ └─┴─┴─┴─┘ └─┴─┴─┴─┘ └─┴─┴...─┴─┴─┘ └─┴─┴...┴─┴─┘
+   u32       u32      u32      [u8; key_len]  [u8; value_len]
 
 Each KV pair is prefixed by 12 bytes which describe its length (key_len + value_len) and its content (checksum)
 
